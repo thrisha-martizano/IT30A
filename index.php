de7ef8985a0cd7b32b5fc5530eb8f134f49d03ff
thrish-martizano <martizanothrisha@gmail.com>
2026-09-17 09:59:55 +0800

    Added student display table on index.php

--- index.php
+++ index.php
@@ -29,6 +29,21 @@ $section = $_GET['section'] ?? 'students';
 //CRUD Operations
 $actions = $_GET['action'] ?? '';
 
+//------------------------------------------
+// Students
+//------------------------------------------
+
+//Fetch students
+if($section === 'students'){
+    $stmt = $pdo->query("
+        SELECT *
+        FROM students
+        ORDER by student_id DESC
+    ");
+
+    $students = $stmt->fetchAll();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -48,6 +63,45 @@ $actions = $_GET['action'] ?? '';
     <hr>
     <?php if($section==='students'): ?>
         <h1>Students</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Course</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($students as $student): ?>
+                    <tr>
+                        <td> 
+                            <?= htmlspecialchars($student['student_id']) ?>
+                        </td>
+                        <td> 
+                            <?= htmlspecialchars($student['student_first_name']) ?>
+                        </td>
+                        <td> 
+                            <?= htmlspecialchars($student['student_last_name']) ?>
+                        </td>
+                        <td> 
+                            <?= htmlspecialchars($student['student_course']) ?>
+                        </td>
+                        <td> 
+                            <?= htmlspecialchars($student['student_created_at']) ?>
+                        </td>
+                        <td> 
+                            <a>Edit</a>
+                            
+                            <a>Delete</a>
+                        </td>
+
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     <?php endif; ?>
 
     <?php if($section==='books'): ?>
